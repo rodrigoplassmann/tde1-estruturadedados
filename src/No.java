4c84f3a1982535985689dadf57b16dfa7d315098
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-09-05 13:04:48 -0300

    Método insere da pilha

---
+++ src/No.java
@@ -0,0 +1,22 @@
+public class No {
+    private Integer informacao;
+    private No proximo;
+
+    public No(){
+        this.informacao = null;
+        this.proximo = null;
+    }
+
+    public Integer getInformacao() {
+        return informacao;
+    }
+    public void setInformacao(Integer informacao) {
+        this.informacao = informacao;
+    }
+    public No getProximo() {
+        return proximo;
+    }
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+}
--- src/Pilha.java
+++ src/Pilha.java
@@ -1,2 +1,14 @@
 public class Pilha {
+    private No topo;
+
+    public Pilha(){
+        this.topo = null;
+    }
+
+    public void Insere(int x){
+        No n = new No();
+        n.setInformacao(x);
+        n.setProximo(topo);
+        topo = n;
+    }
 }
